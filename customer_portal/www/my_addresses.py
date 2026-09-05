4e6700f34fa3ec0e54d038436e70515dfac0428b
pooja <poojac26kutty@gmail.com>
2026-09-05 13:01:17 +0530

    address design commit

--- customer_portal/www/my_addresses.py
+++ customer_portal/www/my_addresses.py
@@ -5,4 +5,25 @@ def get_context(context):
     context = get_base_context(context)
     context.title = "Addresses"
     context.pathname = "/addresses"
+    
+    if not context.customer_id or context.customer_id == "Not Linked":
+        context.addresses = []
+        return context
+
+    # Fetch addresses linked to the customer
+    links = frappe.get_all("Dynamic Link", 
+        filters={"parenttype": "Address", "link_doctype": "Customer", "link_name": context.customer_id},
+        fields=["parent"]
+    )
+    
+    if links:
+        address_names = [d.parent for d in links]
+        addresses = frappe.get_all("Address", 
+            filters={"name": ["in", address_names]},
+            fields=["*"]
+        )
+        context.addresses = addresses
+    else:
+        context.addresses = []
+
     return context
